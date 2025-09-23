60ae232d53d2ea31d1e7b1fc3b859fb03a3342b3
ANA BEATRIZ RAMOS <ana.bramos4@senacsp.edu.br>
2025-09-22 21:28:22 -0300

    Funções em JavaScript

---
+++ AULA-22-09-25/script.js
@@ -0,0 +1,37 @@
+function soma() {
+    let v1 = 200;
+    let v2 = 400;
+    let total = v1 + v2;
+
+    alert("Total = " + total);
+}
+
+function subtracao() {
+    let v1 = 200;
+    let v2 = 400;
+    let total = v1 - v2;
+
+    alert("Total = " + total)
+}
+
+function multiplicacao() {
+    let v1 = 200; 
+    let v2 = 400;
+    let total = v1 * v2;
+
+    alert("Total = " + total)
+}
+
+function divisao() {
+    let v1 = 200;
+    let v2 = 400;
+    let total = v1 / v2;
+
+    alert("Total = " + total)
+}
+
+// É uma boa prática você separar os módulos e importar numa página principal, entra o conceito de clean code e organização para futuras manutenções
+
+
+//prompt()-- exibe caixas de diálogo- Mensagem: Texto q aparece na janela para orientar o usuário - Valor padrão - Opcional 
+// Exemplo: Uma caixa de texto que pede a seguinte informação: "Qual é seu nome? " -- Se o prompt for cancelado,retorna o valor null
